c2b95e535c713bba6bb64e3946a1f9f0d7ed93e7
CROCHARD Tom <crochard.tom@gmail.com>
2024-04-08 15:50:12 +0200

    defilement tems dans store

--- app/src/main/java/com/example/tapcase/StoreFragment.java
+++ app/src/main/java/com/example/tapcase/StoreFragment.java
@@ -1,64 +1,75 @@
 package com.example.tapcase;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.os.Handler;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StoreFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+        import com.example.tapcase.databinding.ActivityClickerBinding;
+        import com.example.tapcase.databinding.FragmentClickerBinding;
+        import com.example.tapcase.databinding.FragmentStoreBinding;
+
+        import java.util.Timer;
+        import java.util.TimerTask;
+
 public class StoreFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private FragmentStoreBinding binding;
+    private double scrollSpeed;
+    private double scrollPos;
     public StoreFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StoreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StoreFragment newInstance(String param1, String param2) {
-        StoreFragment fragment = new StoreFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onResume() {
+        super.onResume();
+        binding.btnStore.setOnClickListener(v -> {
+            scrollPos=0;
+            scrollSpeed=30;
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            scrollPos += scrollSpeed; // Increment the scroll position
+                            if(scrollSpeed>5){
+                                scrollSpeed -= 0.1;
+                            }else{
+                                scrollSpeed -= 0.01;
+                            }
+
+                            binding.tvStore.setText("speed=" + (int) scrollSpeed + " pos=" + (int) scrollPos);
+                            binding.horizontal.scrollTo((int) scrollPos, 0); // Scroll to the new position
+                            if (scrollSpeed <= 0) {
+                                timer.cancel(); // Stop the timer when the activity is destroyed
+                            }
+                        }
+                    });
+                }
+            }, 0, 5); // Exécute la tâche toutes les 1000 millisecondes (1 seconde)
+        });
     }
+
+
+    private Timer timer;
+    private final Handler handler = new Handler();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store, container, false);
+        binding = FragmentStoreBinding.inflate(inflater, container, false);
+
+
+
+
+    
+    
+        return binding.getRoot();
     }
 }
