4411c2e5b241d63412b489f36f75e45836afa39c
Etienne Perez <etienne.perez@reseau.eseo.fr>
2024-04-08 15:50:22 +0200

    Inventaire fonctionnel avec fichier item_data.txt

--- app/src/main/java/com/example/tapcase/Item.java
+++ app/src/main/java/com/example/tapcase/Item.java
@@ -3,9 +3,9 @@ package com.example.tapcase;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,23 +23,27 @@ public class Item extends Fragment {
     private static final String ARG_PRIX = "prix";
     private static final String ARG_RARETE = "rarete";
     private static final String ARG_IMAGE = "image";
+    private static final String ARG_FLEURS_CLIQUE = "fleurs_clique";
 
     private String nom;
     private Integer prix;
-    private Rarete rarete;
+    private Integer fleurs_par_click;
+    private Rarete rarete = Rarete.BASE;
     private Drawable image;
 
+
     public Item() {
-        // Required empty public constructor
+
     }
 
-    public static Item newInstance(String nom, Integer prix, Rarete rarete, Drawable image) {
+    public static Item newInstance(String nom, Integer prix, Rarete rarete, int imageResId, int fleurs_par_click) {
         Item fragment = new Item();
         Bundle args = new Bundle();
         args.putString(ARG_NOM_ARME, nom);
         args.putInt(ARG_PRIX, prix);
         args.putSerializable(ARG_RARETE, rarete);
-        args.putParcelable(ARG_IMAGE, (Parcelable) image);
+        args.putInt(ARG_IMAGE, imageResId);
+        args.putInt(ARG_FLEURS_CLIQUE, fleurs_par_click);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,15 +54,39 @@ public class Item extends Fragment {
         if (getArguments() != null) {
             nom = getArguments().getString(ARG_NOM_ARME);
             prix = getArguments().getInt(ARG_PRIX);
+            fleurs_par_click = getArguments().getInt(ARG_FLEURS_CLIQUE);
             rarete = (Rarete) getArguments().getSerializable(ARG_RARETE);
-            image = getArguments().getParcelable(ARG_IMAGE);
+            image = getResources().getDrawable(getArguments().getInt(ARG_IMAGE));
         }
     }
-
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item, container, false);
+        binding = FragmentItemBinding.inflate(inflater, container, false);
+        binding.ivImageArme.setImageDrawable(image);
+        switch (rarete){
+            case BASE:
+                binding.ivImageArme.setBackground(getResources().getDrawable(R.drawable.arrondir_base));
+                break;
+            case EXOTIQUE:
+                binding.ivImageArme.setBackground(getResources().getDrawable(R.drawable.arrondir_exotique));
+                break;
+            case SUPERIEUR:
+                binding.ivImageArme.setBackground(getResources().getDrawable(R.drawable.arrondir_superieur));
+                break;
+            case CLASSIFIE:
+                binding.ivImageArme.setBackground(getResources().getDrawable(R.drawable.arrondir_classifie));
+                break;
+        }
+        binding.ivImageArme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Inventory.select_weapons(nom, image, rarete, prix, String.valueOf(fleurs_par_click), getResources());
+            }
+        });
+
+        return binding.getRoot();
     }
+
 }
