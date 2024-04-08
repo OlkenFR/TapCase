2ac25ab32b7b8fe65755f7d6c0e18aba4d9f19e0
CROCHARD Tom <crochard.tom@gmail.com>
2024-04-08 09:42:45 +0200

    Version 0.1 Ajout images avec les bons noms

--- app/src/main/java/com/example/tapcase/Clicker.java
+++ app/src/main/java/com/example/tapcase/Clicker.java
@@ -2,6 +2,7 @@ package com.example.tapcase;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,12 +14,24 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Clicker extends AppCompatActivity {
     private ActivityClickerBinding binding;
+    private int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityClickerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        binding.buttonClicker.setOnClickListener(v -> {
+            score = score + 1;
+            binding.scoreClicker.setText("" + score);
+        });
 
     }
 }
