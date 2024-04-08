ae0436e54ca1338ef2a33766defd153e3807bfdb
Etienne Perez <etienne.perez@reseau.eseo.fr>
2024-04-08 08:09:26 +0200

    Ajout des binding dans chaque activité

--- app/src/main/java/com/example/tapcase/Clicker.java
+++ app/src/main/java/com/example/tapcase/Clicker.java
@@ -1,11 +1,15 @@
 package com.example.tapcase;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.tapcase.R;
 
 import com.example.tapcase.databinding.ActivityClickerBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Clicker extends AppCompatActivity {
     private ActivityClickerBinding binding;
@@ -15,5 +19,6 @@ public class Clicker extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityClickerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
     }
 }
